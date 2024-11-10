8161096a4bc0fae70ef02384e10504335e9d7222
Divya Chaudhary <117537394+divyachaudhary099@users.noreply.github.com>
2024-11-11 01:20:29 +0530

    Update script.js
    attached proxy server

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
-const apiKey = '54de5fd7efa64d1b90dd11b0420985fc';
-
+const apiKey = '54de5fd7efa64d1b90dd11b0420985fc';  
 const blogContainer = document.getElementById("blog-container");
 const searchField = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
-async function fetchRandomNews(){
-    try{
+
+async function fetchRandomNews() {
+    try {
+        const proxyUrl = 'https://proxy-server-seven-ochre.vercel.app/';
         const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apiKey}`;
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
         console.log(data);
         return data.articles;
-    } catch(error){
+    } catch (error) {
         console.error("Error fetching random news", error);
-        return[];
+        return [];
     }
 }
 
-searchButton.addEventListener("click",async()=>{
+searchButton.addEventListener("click", async () => {
     const query = searchField.value.trim();
-    if(query !== ""){
-        try{
+    if (query !== "") {
+        try {
             const articles = await fetchNewsQuery(query);
             displayBlogs(articles);
-        }catch(error){
+        } catch (error) {
             console.log("Error fetching news by query", error);
         }
     }
-})
+});
 
-async function fetchNewsQuery(query){
-    try{
+async function fetchNewsQuery(query) {
+    try {
+        const proxyUrl = 'https://proxy-server-seven-ochre.vercel.app/';
         const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=10&apiKey=${apiKey}`;
-        const response = await fetch(apiUrl);
+        const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
         console.log(data);
         return data.articles;
-    } catch(error){
-        console.error("Error fetching random news", error);
-        return[];
+    } catch (error) {
+        console.error("Error fetching news", error);
+        return [];
     }
 }
 
-function displayBlogs(articles){
+function displayBlogs(articles) {
     blogContainer.innerHTML = "";
     articles.forEach((article) => {
         const blogCard = document.createElement("div");
@@ -50,27 +52,27 @@ function displayBlogs(articles){
         img.src = article.urlToImage;
         img.alt = article.title;
         const title = document.createElement("h2");
-        const truncatedTitle = article.title.length > 40 ? article.title.slice(0,40) + "..." : article.title;
+        const truncatedTitle = article.title.length > 40 ? article.title.slice(0, 40) + "..." : article.title;
         title.textContent = truncatedTitle;
         const description = document.createElement("p");
-        const truncatedDes = article.description.length > 100 ? article.description.slice(0,100) + "..." : article.description;
+        const truncatedDes = article.description.length > 100 ? article.description.slice(0, 100) + "..." : article.description;
         description.textContent = truncatedDes;
-        
+
         blogCard.appendChild(img);
         blogCard.appendChild(title);
         blogCard.appendChild(description);
         blogContainer.appendChild(blogCard);
         blogCard.addEventListener('click', () => {
             window.open(article.url, "_blank");
-        })
-    })
+        });
+    });
 }
-(async ()=>{
-    try{
-       const articles = await fetchRandomNews();
-       displayBlogs(articles);
-    }catch(error){
-        console.error("Error fetching random news", error);
 
+(async () => {
+    try {
+        const articles = await fetchRandomNews();
+        displayBlogs(articles);
+    } catch (error) {
+        console.error("Error fetching random news", error);
     }
 })();
